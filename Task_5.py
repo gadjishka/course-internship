f5e803bcb7b03268708aeb2d144a6d370ec02e83
Ballin <32506894+gadjishka@users.noreply.github.com>
2022-06-28 10:08:46 +0300

    Update Task_5.py

--- Task_5.py
+++ Task_5.py
@@ -1,43 +1,18 @@
-from math import sqrt
+from itertools import combinations_with_replacement
+from functools import reduce
 
 def count_find_num(primesL, limit):
-    prod = 1
-    count = 0
-    max_int = 0
-    for i in range(len(primesL)):
-        prod *= primesL[i]
-    for x in range(prod, limit + 1):
-        tmp = set()
-        x_2 = x
-        for i in range(2, int(sqrt(x) + 1)):
-            while x % i == 0:
-                tmp.add(int(i))
-                x /= i
-        if x != 1:
-            tmp.add(int(x))
-        if tmp == set(primesL):
-            count += 1
-            if x_2 > max_int:
-                max_int = x_2
-    return [count, max_int] if count > 0 else []
-
-primesL = [2, 3]
-limit = 200
-assert count_find_num(primesL, limit) == [13, 192]
-
-primesL = [2, 5]
-limit = 200
-assert count_find_num(primesL, limit) == [8, 200]
-
-primesL = [2, 3, 5]
-limit = 500
-assert count_find_num(primesL, limit) == [12, 480]
-
-primesL = [2, 3, 5]
-limit = 1000
-assert count_find_num(primesL, limit) == [19, 960]
-
-primesL = [2, 3, 47]
-limit = 200
-assert count_find_num(primesL, limit) == []
-
+    ans = set()
+    len_p = len(primesL)
+    for i in range(len_p, 100):
+        f = False
+        tmp = combinations_with_replacement(primesL, i)
+        for _ in tmp:
+            if set(_) == set(primesL):
+                mat_a = reduce(lambda x, y: x * y, _)
+                if mat_a <= limit:
+                    f = True
+                    ans.add(mat_a)
+        if not (f):
+            break
+    return [len(ans), max(ans)] if len(ans) > 0 else []
