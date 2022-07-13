64e4baf369ef1a6104a0c565c9fe95f0f79db022
Ballin <32506894+gadjishka@users.noreply.github.com>
2022-07-13 14:32:09 +0300

    Update places.py

--- HM_3/HM_3_2/Task_Superhero/places.py
+++ HM_3/HM_3_2/Task_Superhero/places.py
@@ -1,14 +1,24 @@
-class Kostroma:
+from abc import ABC, abstractmethod
+
+
+class Place(ABC):
+    @staticmethod
+    @abstractmethod
+    def get_place():
+        pass
+
+
+class Kostroma(Place):
     name = 'Kostroma'
 
     @staticmethod
-    def get_orcs():
+    def get_place():
         print('Orcs hid in the forest')
 
 
-class Tokyo:
+class Tokyo(Place):
     name = 'Tokyo'
 
     @staticmethod
-    def get_godzilla():
+    def get_place():
         print('Godzilla stands near a skyscraper')
