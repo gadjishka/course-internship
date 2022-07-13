21767f3148522bd7e3081c0aad632965b8f1bcfd
Ballin <32506894+gadjishka@users.noreply.github.com>
2022-07-13 13:39:51 +0300

    Create places.py

---
+++ HM_3/HM_3_2/Task_Superhero/places.py
@@ -0,0 +1,14 @@
+class Kostroma:
+    name = 'Kostroma'
+
+    @staticmethod
+    def get_orcs():
+        print('Orcs hid in the forest')
+
+
+class Tokyo:
+    name = 'Tokyo'
+
+    @staticmethod
+    def get_godzilla():
+        print('Godzilla stands near a skyscraper')
