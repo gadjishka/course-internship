35f4aecbafb5efe50212ed3749a56d2eea09b2a6
Ballin <32506894+gadjishka@users.noreply.github.com>
2022-07-13 14:32:37 +0300

    Update antagonistfinder.py

--- HM_3/HM_3_2/Task_Superhero/antagonistfinder.py
+++ HM_3/HM_3_2/Task_Superhero/antagonistfinder.py
@@ -1,17 +1,7 @@
 from places import Kostroma, Tokyo
-from abc import ABC, abstractmethod
 
 
-class Place(ABC):
-    @abstractmethod
-    def get_antagonist(self, place):
-        if isinstance(place, Kostroma):
-            place.get_orcs()
-        elif isinstance(place, Tokyo):
-            place.get_godzilla()
-
-
-class AntagonistFinder(Place):
+class AntagonistFinder:
 
     def get_antagonist(self, place):
-        super(AntagonistFinder, self).get_antagonist(place)
+        place.get_place()
